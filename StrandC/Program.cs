0a9409aa844739e7c7f007ae9e09411a886a85f0
raczl <raczl@kkszki.hu>
2026-05-04 14:57:03 +0200

    8 feladat

--- StrandC/Program.cs
+++ StrandC/Program.cs
@@ -12,7 +12,14 @@ namespace StrandC
             }
             Console.WriteLine("7 feladat:");
             Console.WriteLine($"Fűrdők száma : {list.Count}");
+            int osszeg = 0;
+            foreach (var item in list)
+            {
+                osszeg += item.Ár;
 
+            }
+            double átlag = osszeg / (double)list.Count;
+            Console.WriteLine($"8. Feladat \r\n A Furdobelepok átlagos ára {átlag:F1} \r\n");
         }
     }
 }
